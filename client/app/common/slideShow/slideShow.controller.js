b6c2b35ba1190193e71bd4d0258568ce39e5d3b5
Hao Huynh <minhhao411@gmail.com>
2016-10-13 05:53:44 +0700

    bugs fix

--- client/app/common/slideShow/slideShow.controller.js
+++ client/app/common/slideShow/slideShow.controller.js
@@ -4,37 +4,44 @@ class SlideShowController {
   constructor($interval) {
     this.interval = $interval;
   }
+  
   $onInit() {
-    this.slides = [];
-    this.generateSlides(this.count);
-
+    this.slides = this.generateSlides(this.count);
     this.currentIndex = 0;
-    this.setCurrentSlideIndex = function (index) {
-      this.currentIndex = index;
-    };
-    this.isCurrentSlideIndex = function (index) {
-      return this.currentIndex === index;
-    };
-
-    this.prevSlide = function () {
-      this.currentIndex = (this.currentIndex > 0) ? --this.currentIndex : this.slides.length - 1;
-    };
-    this.nextSlide = function () {
-      this.currentIndex = (this.currentIndex < this.slides.length - 1) ? ++this.currentIndex : 0;
-    };
-
-    this.stopTime = this.interval(()=> this.nextSlide(), 5000);
+    //auto play
+    //this.stopTime = this.interval(()=> this.nextSlide(), 5000);
   }
+
   $onDestroy(){
     this.interval.cancel(this.stopTime);
   }
 
+  prevSlide(){
+    this.prev = true;
+    this.currentIndex = (this.currentIndex > 0) ? --this.currentIndex : this.slides.length - 1;
+  }
+
+  nextSlide(){
+    this.prev = false;
+    this.currentIndex = (this.currentIndex < this.slides.length - 1) ? ++this.currentIndex : 0;
+  }
+
+  isCurrentSlideIndex(index){
+    return this.currentIndex === index;
+  }
+
+  setCurrentSlideIndex(index){
+    this.prev = index < this.currentIndex;
+    this.currentIndex = index;
+  }
 
   generateSlides(count = 10){
+    let slides = [];
     for (var i = 0; i < count; i++) {
       let slide = new Slide();
-      this.slides.push(slide);
+      slides.push(slide);
     }
+    return slides;
   }
 }
 
